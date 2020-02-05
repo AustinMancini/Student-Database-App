32d954f19a225626cd6b34fc372356d04c9fe857
AustinMancini <mancini787@yahoo.com>
2020-02-05 14:36:22 -0500

    version 1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# Java Student Database Application
+
+This app simulates a management system for new student enrollments and tuition balance.
+
+## Features
+* Adds students to a database
+* Creates a unique 7-digit ID with the first number being their grade level
+* Enrolls students in proper courses
+
+
+## Built With
+*   Java SE 11
+
+
+## Contributors
+Austin Mancini - *Initial work* - http://amancini.dev/
---
+++ src/studentdatabaseapp/Student.java
@@ -0,0 +1,75 @@
+package studentdatabaseapp;
+import java.util.Scanner;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private String studentID;
+    private String courses = "";
+    private int tuitionBalance;
+    private static int id = 1000;
+
+    // Constructor: prompt user to enter student's name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter students first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter students last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = in.nextInt();
+
+        setStudentID();
+    }
+
+    // Generate ID
+    private void setStudentID() {
+        // Grade Level + ID
+        id++;
+        this.studentID = gradeYear + "" + id;
+    }
+
+    // Enroll in courses
+    public void enroll() {
+        // Loop until user quits
+        while (true) {
+            System.out.print("Enter a course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+
+            // Get input from user
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses +=  "\n  " + course;
+                int costOfCourse = 600;
+                tuitionBalance += costOfCourse;
+            } else { break; }
+        }
+    }
+
+    // View balance
+    public void viewBalance() {
+        System.out.println("Your balance is: $" + tuitionBalance);
+    }
+
+    // Make a payment
+    public void payTuition() {
+        System.out.print("Enter your payment: $");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of $" + payment);
+        viewBalance();
+    }
+
+    // Show status
+    public String showInfo() {
+        return "\nName: " + firstName + " " + lastName +
+                "\nGrade Level: " + gradeYear +
+                "\nStudent ID: " + studentID +
+                "\nCourses Enrolled: " + courses +
+                "\nBalance: $" + tuitionBalance;
+    }
+}
